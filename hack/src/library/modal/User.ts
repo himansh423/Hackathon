aab9bf7b210b21592b662308f2c1608f78e1872a
Himanshu Chauhan <himanshuchau423@gmail.com>
2025-02-16 12:17:01 +0530

    authetication API done

---
+++ hack/src/library/modal/User.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    type: String,
+  },
+  isVerified: {
+    type: Boolean,
+  },
+});
+
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
+
+export default User;
